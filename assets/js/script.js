b7d106a27bd03c9415b9f0256ceb0076179e1eba
FabianLizana <fabianlizana1870@gmail.com>
2024-02-16 20:56:20 -0300

    Quinto commit arreglando el footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,51 @@
-//Funcion que cambia color con scroll
+//Funcion que cambia color con scroll//
 
-$(window).scroll(function(){
+// $(window).scroll(function() {
+//     if ($(this).scrollTop() > 400) {
+//       $("#menu").removeClass("navbar-dark");
+//       $("#menu").addClass("navbar-light bg-interno");
+//     } else {
+//       $("#menu").removeClass("navbar-light bg-interno");
+//       $("#menu").addClass("navbar-dark");
+//     }
+//   });
 
-    if ($(this).scrollTop() > 400) {
-        $("#menu").removeClass("navbar");
-       $('#menu').addClass("bg-interno");
-      } else {
 
-       $("#menu").removeClass("bg-interno");
-       $('#menu').addClass("navbar");
-      }
+$(document).ready(function () {
+    var navBar = $('#menu');
 
-   });
+    $(window).scroll(function () {
+        console.log($(this).scrollTop()); // Agrega esta línea para verificar el valor de scrollTop
+
+        if ($(this).scrollTop() > 500) {
+            navBar.removeClass('navbar-light');
+            navBar.addClass('bg-interno navbar-dark');
+        } else {
+            navBar.removeClass('bg-interno navbar-dark');
+            navBar.addClass('navbar-light');
+        }
+    });
+});
+
+
+  $(document).ready(function () {
+    // Selecciona todos los enlaces de la barra de navegación que tienen un href que comienza con #
+    $('a[href^="#"]').on('click', function (e) {
+      e.preventDefault();
+
+      // Obtiene el atributo href del enlace
+      var target = this.hash;
+      var $target = $(target);
+
+      // Desplaza la página hacia el objetivo con animación
+      $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+      }, 900, 'swing', function () {
+        window.location.hash = target;
+      });
+    });
+  });
+
 
 //    $(window).scroll(function(){
 //     var $menu = $("#menu");
