194e8ec6ed6a0498637ca8a23e353fce44e7c9f1
FabianLizana <fabianlizana1870@gmail.com>
2024-02-17 00:52:32 -0300

    Septimo commit con ultimos cambios en la pagina

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,29 @@
-//Funcion que cambia color con scroll//
-
 $(document).ready(function () {
-    var navBar = $('#menu');
+  var navBar = $('#menu');
 
-    $(window).scroll(function () {
-        console.log($(this).scrollTop()); // Agrega esta línea para verificar el valor de scrollTop
+  // Cambia el color de la barra de navegación con el desplazamiento
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 500) {
+          navBar.removeClass('navbar-light').addClass('bg-interno navbar-dark');
+      } else {
+          navBar.removeClass('bg-interno navbar-dark').addClass('navbar-light');
+      }
+  });
 
-        if ($(this).scrollTop() > 500) {
-            navBar.removeClass('navbar-light');
-            navBar.addClass('bg-interno navbar-dark');
-        } else {
-            navBar.removeClass('bg-interno navbar-dark');
-            navBar.addClass('navbar-light');
-        }
-    });
-});
-
-  $(document).ready(function () {
-    // Selecciona todos los enlaces de la barra de navegación que tienen un href que comienza con #
-    $('a[href^="#"]').on('click', function (e) {
+  // Desplazamiento suave al hacer clic en enlaces de la barra de navegación
+  $('a[href^="#"]').on('click', function (e) {
       e.preventDefault();
 
-      // Obtiene el atributo href del enlace
       var target = this.hash;
       var $target = $(target);
 
-      // Desplaza la página hacia el objetivo con animación
       $('html, body').stop().animate({
-        'scrollTop': $target.offset().top
+          'scrollTop': $target.offset().top
       }, 900, 'swing', function () {
-        window.location.hash = target;
+          window.location.hash = target;
       });
-    });
   });
+});
+
 
 
